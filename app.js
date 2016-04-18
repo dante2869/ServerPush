27530476f369e097e4128238cd62de24597ea5c8
Oscar Daniel Arellano M <oscardanielarellano@gmail.com>
2016-04-18 15:47:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hola al demo de server push!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
